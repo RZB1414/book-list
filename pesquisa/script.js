b552aa4c94ab42e7b8242f86bd83c416496565c3
Renan Buiatti <renanbuiatti14@gmail.com>
2023-08-22 16:54:28 +0300

    removed delete from book`s context menu and cannot add the same book more then 1 time

--- pesquisa/script.js
+++ pesquisa/script.js
@@ -2,117 +2,125 @@ const input = document.querySelector('.pesquisa-input');
 const button = document.querySelector('.pesquisa-botao');
 const menuList = document.querySelector('.livro-menu');
 
-button.addEventListener('click', (event) => {
-    event.preventDefault();
+button.addEventListener('click', evento => buscaLivros(evento));
 
+//requisição para a API do google
+async function buscaLivros(evento) {
+    evento.preventDefault();
     const text = input.value;
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${text}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const livros = data.items;
-            const livrosContainer = document.querySelector('.livros-container');
-            livrosContainer.innerHTML = '';
-            livros.forEach(livro => {
-                const livroContainer = document.createElement('div');
-                livroContainer.classList.add('livro-container');
-                const tituloContainer = document.createElement('div');
-                tituloContainer.classList.add('livro-titulo-container');
-                const titulo = document.createElement('h2');
-                titulo.classList.add('livro-titulo');
-                titulo.textContent = livro.volumeInfo.title;
 
-                const divCheckbox = document.createElement('div');
-                divCheckbox.classList.add('container__checkbox');
-                const checkBox = document.createElement('input');
-                checkBox.type = 'checkbox';
-                checkBox.id = 'menu';
-                checkBox.classList.add('container__botao');
-                const label = document.createElement('label');
-                label.setAttribute('for', 'menu');
-                label.classList.add('container__rotulo');
-                const span = document.createElement('span');
-                span.classList.add('cabeçalho__menu-hamburguer');
-                label.appendChild(span);
-                divCheckbox.appendChild(checkBox);
-                divCheckbox.appendChild(label);
+    const conexaoGoogle = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${text}`);
+    const conexaoGoogleConvertida = await conexaoGoogle.json();
 
-                span.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    checkBox.classList.toggle('active');
-                    listaMenu.style.display = checkBox.classList.contains('active') ? 'block' : 'none';
+    const livros = conexaoGoogleConvertida.items;
+
+    //limpa a div de livros
+    const livrosContainer = document.querySelector('.livros-container');
+    livrosContainer.innerHTML = '';
+
+    //cria os elementos de cada livro
+    livros.forEach(livro => {
+        const livroContainer = document.createElement('div');
+        livroContainer.classList.add('livro-container');
+        const tituloContainer = document.createElement('div');
+        tituloContainer.classList.add('livro-titulo-container');
+        const titulo = document.createElement('h2');
+        titulo.classList.add('livro-titulo');
+        titulo.textContent = livro.volumeInfo.title;
+
+        //cria o menu de cada livro
+        const divCheckbox = document.createElement('div');
+        divCheckbox.classList.add('container__checkbox');
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.id = 'menu';
+        checkBox.classList.add('container__botao');
+        const label = document.createElement('label');
+        label.setAttribute('for', 'menu');
+        label.classList.add('container__rotulo');
+        const span = document.createElement('span');
+        span.classList.add('cabeçalho__menu-hamburguer');
+        label.appendChild(span);
+        divCheckbox.appendChild(checkBox);
+        divCheckbox.appendChild(label);
+
+        //evento para abrir o menu
+        span.addEventListener('click', (event) => {
+            event.preventDefault();
+            checkBox.classList.toggle('active');
+            listaMenu.style.display = checkBox.classList.contains('active') ? 'block' : 'none';
+        });
+
+        //cria a lista de opções do menu
+        const listaMenu = document.createElement('ul');
+        listaMenu.classList.add('lista-menu');
+        const itemMenu = document.createElement('li');
+        itemMenu.classList.add('lista-menu-item');
+
+        //evento para adicionar o livro
+        const linkMenu = document.createElement('p');
+        linkMenu.classList.add('lista-menu-link');
+        linkMenu.href = '#';
+        linkMenu.textContent = 'Add';
+        itemMenu.classList.add('lista-menu-item');
+
+        //função para adicionar o livro
+        async function addLivro(evento) {
+            evento.preventDefault();
+
+            //verifica se o livro já está na biblioteca
+            const conexaoIf = await fetch('http://localhost:3000/livros');
+            const conexaoIfConvertida = await conexaoIf.json();
+            const livroExiste = conexaoIfConvertida.some(livros => livro.volumeInfo.title === livros.titulo);
+
+            if (livroExiste) {
+                alert('This book is alredy in your library!');
+            } else {
+                //add o livro na biblioteca
+                evento.preventDefault();
+                const conexao = await fetch('http://localhost:3000/livros', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        titulo: livro.volumeInfo.title,
+                        descricao: livro.volumeInfo.description,
+                        imagem: livro.volumeInfo.imageLinks.thumbnail,
+                        link: livro.volumeInfo.previewLink
+                    })
                 });
+            }
+        }
 
-                const listaMenu = document.createElement('ul');
-                listaMenu.classList.add('lista-menu');
-                const itemMenu1 = document.createElement('li');
-                itemMenu1.classList.add('lista-menu-item');
+        linkMenu.addEventListener('click', evento => {
+            evento.preventDefault();
+            addLivro(evento)});
 
-                const linkMenu1 = document.createElement('p');
-                linkMenu1.classList.add('lista-menu-link');
-                linkMenu1.href = '#';
-                linkMenu1.textContent = 'Add';
+        //appends
+        itemMenu.appendChild(linkMenu);
+        listaMenu.appendChild(itemMenu);
+        tituloContainer.appendChild(titulo);
+        tituloContainer.appendChild(divCheckbox);
+        tituloContainer.appendChild(listaMenu);
 
-                linkMenu1.addEventListener('click', async (event) => {
-                    event.preventDefault();
-                    await addLivro();
-                    console.log(livro);
-                    async function addLivro() {
-                        const conexao = await fetch('http://localhost:3000/livros', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                titulo: livro.volumeInfo.title,
-                                descricao: livro.volumeInfo.description,
-                                imagem: livro.volumeInfo.imageLinks.thumbnail,
-                                link: livro.volumeInfo.previewLink
-                            })
-                        });
-                        const conexaoConvertida = await conexao.json();
-                        return conexaoConvertida;
-                    }
+        //cria os elementos de cada livro
+        const descricao = document.createElement('p');
+        descricao.classList.add('livro-descricao');
+        descricao.textContent = livro.volumeInfo.description;
+        const imagem = document.createElement('img');
+        imagem.classList.add('livro-imagem');
+        imagem.src = livro.volumeInfo.imageLinks.thumbnail;
+        const link = document.createElement('a');
+        link.classList.add('livro-link');
+        link.href = livro.volumeInfo.previewLink;
+        link.textContent = 'Read more...';
 
-                    
-                })
-
-
-
-
-
-                const itemMenu2 = document.createElement('li');
-                itemMenu1.classList.add('lista-menu-item');
-
-                const linkMenu2 = document.createElement('a');
-                linkMenu2.classList.add('lista-menu-link');
-                linkMenu2.href = '#';
-                linkMenu2.textContent = 'Remove';
-
-                itemMenu1.appendChild(linkMenu1);
-                itemMenu2.appendChild(linkMenu2);
-                listaMenu.appendChild(itemMenu1);
-                listaMenu.appendChild(itemMenu2);
-
-
-                tituloContainer.appendChild(titulo);
-                tituloContainer.appendChild(divCheckbox);
-                tituloContainer.appendChild(listaMenu);
-                const descricao = document.createElement('p');
-                descricao.classList.add('livro-descricao');
-                descricao.textContent = livro.volumeInfo.description;
-                const imagem = document.createElement('img');
-                imagem.classList.add('livro-imagem');
-                imagem.src = livro.volumeInfo.imageLinks.thumbnail;
-                const link = document.createElement('a');
-                link.classList.add('livro-link');
-                link.href = livro.volumeInfo.previewLink;
-                link.textContent = 'Read more...';
-                livroContainer.appendChild(tituloContainer);
-                livroContainer.appendChild(imagem);
-                livroContainer.appendChild(descricao);
-                livroContainer.appendChild(link);
-                livrosContainer.appendChild(livroContainer);
-            })
-        })
-})
+        //appends
+        livroContainer.appendChild(tituloContainer);
+        livroContainer.appendChild(imagem);
+        livroContainer.appendChild(descricao);
+        livroContainer.appendChild(link);
+        livrosContainer.appendChild(livroContainer);
+    })
+}
